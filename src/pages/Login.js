48453f13044b491527293b4880425da6da5a1e53
Javier Miz Arévalo <jmizarevalo@gmail.com>
2021-06-18 18:29:19 -0500

    Controlled components for Inputs

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,45 +1,61 @@
-import React, { useRef, useState } from "react"
+import React, { useState } from "react"
 import { useAuth } from "../contexts/AuthContext"
 import { Link, useHistory } from "react-router-dom"
-import Label from "../components/Label"
+import { Label } from "../components/atoms/Label"
+import { Input } from "../components/atoms/Input"
+import { Form } from "../components/atoms/Form"
 
 const Login = () => {
-  const emailRef = useRef()
-  const passwordRef = useRef()
   const { login } = useAuth()
   const [error, setError] = useState("")
   const [loading, setLoading] = useState(false)
   const history = useHistory()
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  })
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault()
-
     try {
-      setError("")
-      setLoading(true)
-      await login(emailRef.current.value, passwordRef.current.value)
-      history.push("/admin")
-      setLoading(false)
+      loginAndRedirect();
     } catch {
       setError("Failed to log in")
       setLoading(false)
     }
+  }
 
+  const handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    setFormValues({ ...formValues, [name]: value })
+  }
+
+  const loginAndRedirect = async () => {
+    setError("")
+    setLoading(true)
+    await login(formValues.email, formValues.password)
+    history.push("/admin")
+    setLoading(false)
   }
 
   return (
     <>
       <h2>Log In</h2>
       {error && <span variant="danger">{error}</span>}
-      <form onSubmit={handleSubmit}>
-        <Label>Email</Label>
-        <input type="email" ref={emailRef} required />
-        <label>Password</label>
-        <input type="password" ref={passwordRef} required />
+      <Form onSubmit={handleSubmit}>
+        <Label bg={"#000"}>Email</Label>
+        <Input name="email" type="email" required onChange={handleInputChange} />
+
+        <Label>Password</Label>
+        <Input name="password" type="password" required onChange={handleInputChange} />
+
         <button disabled={loading} type="submit">
           Log In
         </button>
-      </form>
+      </Form>
       <Link to="/forgot-password">Forgot Password?</Link>
     </>
   )
